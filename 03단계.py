678a94788777b536add41fb848be73212675578a
SoYeon Yang <85726134+xoyeon@users.noreply.github.com>
2022-01-07 15:19:12 +0900

    for문

--- 03단계.py
+++ 03단계.py
@@ -30,15 +30,47 @@ for i in range(t):
     print(a+b)
     
 # 5	2741	 N 찍기
+n = int(input())
+
+for i in range(1,n+1):
+    print(i)
 
 # 6	2742	 기찍 N
+n = int(input())
 
+for i in range (n, 0, -1):
+    print(i)
+    
 # 7	11021	 A+B - 7
+T = int(input())
+
+for i in range (1, T+1):
+    a, b = map(int, input().split())
+    print("Case #"+str(i)+":", a + b)
 
 # 8	11022	 A+B - 8
+T = int(input())
+
+for i in range(1, T+1):
+    a, b = map(int, input().split())
+    print(f"Case #{i}: {a} + {b} = {a+b}")
 
 # 9	2438	 별 찍기 - 1
+n = int(input())
+
+for i in range(1, n+1):
+    print("*"*i)
 
 # 10	2439	 별 찍기 - 2
+n = int(input())
+
+for i in range(1, n+1):
+    print(" "*(n-i)+"*"*i)
 
 # 11	10871	 X보다 작은 수
+n, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+for i in range(n):
+    if a[i] < x:
+        print(a[i])
