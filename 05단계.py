a558c674c5a9ae9750681a7d5200b311495e5cf8
SoYeon Yang <85726134+xoyeon@users.noreply.github.com>
2022-01-10 21:43:45 +0900

    1차원 배열

---
+++ 05단계.py
@@ -0,0 +1,12 @@
+# 1	10818	 최소, 최대
+n = int(input())
+num = list(map(int, input().split()))
+
+print(min(num), max(num))
+
+# 2	2562	 최댓값
+# 3	2577	 숫자의 개수
+# 4	3052	 나머지
+# 5	1546	 평균	
+# 6	8958	 OX퀴즈
+# 7	4344	 평균은 넘겠지
