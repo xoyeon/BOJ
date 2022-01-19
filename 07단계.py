0669a7963df217508c7675040d836edb07b6a208
SoYeon Yang <85726134+xoyeon@users.noreply.github.com>
2022-01-19 16:00:50 +0900

    문자열

--- 07단계.py
+++ 07단계.py
@@ -45,9 +45,42 @@ words = input().split()
 print(len(words))
 
 # 7	2908	 상수
+a, b = map(str, input().split())
+a = a[::-1]
+b = b[::-1]
+
+if a > b:
+  print(a)
+else:
+  print(b)
 
 # 8	5622	 다이얼
+word = input()
+phone = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+
+answer = 0
+for i in range(len(word)):
+  for j in phone:
+    if word[i] in j:
+      answer+= phone.index(j)+3
+print(answer)
 
 # 9	2941	 크로아티아 알파벳
+change = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in change:
+  word = word.replace(i, '*')
+print(len(word))
 
 # 10	1316	 그룹 단어 체커
+n = int(input())
+count = n
+
+for i in range(n):
+  word = input()
+  for j in range(len(word)-1):
+    if word.find(word[j]) > word.find(word[j+1]) :
+      count-=1
+      break
+print(count)
