06a55b0763db3d9a50b7a822015a97ebe5fffcce
SoYeon Yang <85726134+xoyeon@users.noreply.github.com>
2022-01-28 13:14:28 +0900

    정렬

---
+++ 12단계.py
@@ -0,0 +1,20 @@
+# 1	2750	 수 정렬하기
+n = int(input())
+
+num=[]
+for i in range(n):
+    num.append(int(input()))
+num_sort = sorted(num)
+
+for i in range(len(num)):
+               print(num_sort[i])
+    
+# 2	2751	 수 정렬하기 2
+# 3	10989	 수 정렬하기 3
+# 4	2108	 통계학
+# 5	1427	 소트인사이드
+# 6	11650	 좌표 정렬하기
+# 7	11651	 좌표 정렬하기 2
+# 8	1181	 단어 정렬
+# 9	10814	 나이순 정렬
+# 10	18870	 좌표 압축
